8316f21030dac6ace304225cb2ca5966b15cf790
Brandon H Bodine <brandonhbodine@gmail.com>
2016-05-04 17:02:40 -0600

    Converting HTML to jade and integrating it into wintersmith. Will be consulting Jroel on what direction he wants to go witht the deisgn, specifically the header and footer.

--- contents/js/slack-invite.js
+++ contents/js/slack-invite.js
@@ -1,6 +1,5 @@
 (function() {
     'use strict';
-
     var form = document.getElementById('slack');
     var email = document.getElementById('slack-email');
     form.addEventListener('submit', function(evt) {
